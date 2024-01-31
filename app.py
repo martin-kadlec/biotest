300df139d8337e787389f99d831f89395dffe23f
Martin Kadlec <martin.kadlec@aon.com>
2024-01-31 14:55:26 +0100

    random image working

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask, render_template, request, url_for, redirect
-from models import db, Test
+from models import db, Guess, Image
+from pathlib import Path
+import random
 
 # init app and db
 
@@ -22,8 +24,29 @@ def init_db():
     with app.app_context():
         db.create_all()
 
+def add_images():
+    with app.app_context():
+        img_path = Path(os.path.dirname(__file__)) / "img"
+        images = [i.name for i in img_path.glob("*.png")]
+        db_images = set((i.filename for i in Image.query.all()))
+
+        for image in images:
+            if not image in db_images:
+                new = Image()
+                new.filename = image
+                new.title = f"unknown - {image}"
+                db.session.add(new)
+
+        db.session.commit()
+
 # views
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/rand")
+def rand():
+    images = Image.query.all()
+    rimg = images[random.randint(0, len(images)-1)]
+    return render_template("guess.html", img=rimg)
--- models.py
+++ models.py
@@ -1,13 +1,32 @@
 from sqlalchemy.orm import Mapped, mapped_column
+from typing import List
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, relationship
+from datetime import datetime
+from sqlalchemy.sql import func
+from sqlalchemy import ForeignKey
 
 class Base(DeclarativeBase):
     pass
 
 db = SQLAlchemy(model_class=Base)
 
-class Test(db.Model):
+class Guess(db.Model):
+    __tablename__ = "guess"
+
+    # Column(DateTime(timezone=True), server_default=func.now())
     id: Mapped[int] = mapped_column(primary_key=True)
-    username: Mapped[str] = mapped_column(unique=True)
-    email: Mapped[str]
+    created: Mapped[datetime] = mapped_column(server_default=func.now())
+    image_id: Mapped[int] = mapped_column(ForeignKey("image.id"))
+
+    image: Mapped["Image"] = relationship(back_populates="guesses")
+
+
+class Image(db.Model):
+    __tablename__ = "image"
+
+    id: Mapped[int] = mapped_column(primary_key=True)    
+    title: Mapped[str] = mapped_column(default="unknown")
+    filename: Mapped[str] = mapped_column(unique=True)
+
+    guesses: Mapped[List["Guess"]] = relationship(back_populates="image")
