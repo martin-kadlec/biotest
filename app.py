33438d930098a3f733ba69699c9e1b28d92c2985
Martin Kadlec <martin.kadlec@aon.com>
2024-01-31 07:59:08 +0100

    -

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import os
 from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 
-
-
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
