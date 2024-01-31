aa1aac5c8efe7ebc926efec201edd95098f9b53e
Martin Kadlec <martin.kadlec@aon.com>
2024-01-31 09:54:04 +0100

    model refactoring

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
 import os
 from flask import Flask, render_template, request, url_for, redirect
-from flask_sqlalchemy import SQLAlchemy
+from models import db, Test
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+# init app and db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] =\
-        'sqlite:///' + os.path.join(basedir, 'database.db')
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db.init_app(app)
+
+# init db
+def reset_db():
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
+
+def init_db():
+    with app.app_context():
+        db.create_all()
+
+# views
 
 @app.route("/")
 def index():
--- models.py
+++ models.py
@@ -1,34 +1,13 @@
-from sqlalchemy import ForeignKey
-from sqlalchemy import func
-from sqlalchemy import Integer
-from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
-from sqlalchemy.orm import relationship
-from datetime import datetime
-from typing import Optional, List
 
 class Base(DeclarativeBase):
     pass
 
-class User(Base):
-    __tablename__ = "user"
+db = SQLAlchemy(model_class=Base)
 
-    id = mapped_column(Integer, primary_key=True)
-    name: Mapped[str]
-    fullname: Mapped[Optional[str]]
-    nickname: Mapped[Optional[str]] = mapped_column(String(64))
-    create_date: Mapped[datetime] = mapped_column(insert_default=func.now())
-
-    addresses: Mapped[List["Address"]] = relationship(back_populates="user")
-
-
-class Address(Base):
-    __tablename__ = "address"
-
-    id = mapped_column(Integer, primary_key=True)
-    user_id = mapped_column(ForeignKey("user.id"))
-    email_address: Mapped[str]
-
-    user: Mapped["User"] = relationship(back_populates="addresses")
+class Test(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True)
+    email: Mapped[str]
